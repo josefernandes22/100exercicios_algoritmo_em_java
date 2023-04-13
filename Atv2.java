1b0b463b2bc6b188ce36a414bed3a5889d91b4df
josefernandes22 <jcastrosa22@gmail.com>
2023-04-12 21:02:32 -0300

    Atividade 2

---
+++ Atv2.java
@@ -0,0 +1,13 @@
+/*2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela:*/
+
+import java.util.Scanner;
+public class Atv2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nome;
+
+        System.out.println("Qual o seu nome?");
+        nome = scanner.nextLine();
+        System.out.println("Olá " + nome+ " é um prazer te conheçer!");
+    }
+}
