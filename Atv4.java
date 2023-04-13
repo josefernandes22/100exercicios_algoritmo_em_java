d81ea4c69dadcda3f7abf106f83ec8bc296800f9
josefernandes22 <jcastrosa22@gmail.com>
2023-04-12 21:24:44 -0300

    Atividade 4

--- Atv4.java
+++ Atv4.java
@@ -12,8 +12,7 @@ public class Atv4 {
         num1 = scanner.nextInt();
         System.out.println("Digite o segundo numero:");
         num2 = scanner.nextInt();
-
-
+        
         soma = num1+num2;
         System.out.println("A soma de "+num1+" + "+num2+" é igual a: "+soma);
     }
