6d3b0a9388d077eae5347f1b1bf6afa916b94896
josefernandes22 <jcastrosa22@gmail.com>
2023-04-14 20:28:26 -0300

    Atividade 7

--- Atv7.java
+++ Atv7.java
@@ -1,2 +1,17 @@
+/*7) Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a
+sua terça parte.*/
+
+import java.util.Scanner;
 public class Atv7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double numero;
+
+        System.out.println("Digite um numero real:");
+        numero = scanner.nextDouble();
+
+        System.out.printf("DOBRO = %.2f%n", numero*2);
+        System.out.printf("TERÇO = %.2f", numero/3);
+    }
 }
