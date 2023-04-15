293ea40b657dc7ff6672cf55dbf3445b8fa972ec
josefernandes22 <jcastrosa22@gmail.com>
2023-04-14 22:09:34 -0300

    Atividade 10

--- Atv10.java
+++ Atv10.java
@@ -3,7 +3,6 @@ mostre a área a ser pintada e a quantidade de tinta necessária para o serviço
 sabendo que cada litro de tinta pinta uma área de 2metros quadrados.*/
 
 import java.util.Scanner;
-
 public class Atv10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
